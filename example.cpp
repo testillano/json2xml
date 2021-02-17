25ecaa1989002e50fb87bf3badce0be43add9ddb
Eduardo Ramos Testillano (eramedu) <eduardo.ramos.testillano@ericsson.com>
2021-02-17 20:35:36 +0100

    Extract header-only sax consumer from main example
    Also,
    
    - Add README contributing section suggesting about
    astyle formatting (default `.astylerc` provided by
    `frankwolf/astyle` astyle docker image.
    
    - Remove log commented section used for troubleshooting,
    as the parser is now quite stable.

---
+++ example.cpp
@@ -0,0 +1,65 @@
+#include <iostream>
+#include <fstream>
+
+#include "json2xml.hpp"
+
+
+int main(int argc, char *argv[])
+{
+    if (argc != 2) {
+        std::cerr << "Usage: json2xml <json file|test: harcoded test>" << std::endl;
+        return 1;
+    }
+
+    std::string argument = argv[1];
+    std::string jsonStr;
+
+    if (argument != "test") {
+        std::ifstream ifs(argument);
+        std::stringstream ss;
+        ss << ifs.rdbuf();
+        jsonStr = ss.str();
+        if (jsonStr == "") {
+            std::cerr << "Cannot read file provided !" << std::endl;
+            return 1;
+        }
+    }
+    else {
+        jsonStr = R"(
+{
+    "grandfather": {
+        "father": {
+            "child": [
+                {
+                    "@name": "Jose",
+                    "@bestFriend": "Maria"
+                },
+                {
+                    "@name": "Maria",
+                    "@bestFriend": "Jose"
+                }
+            ],
+            "student": {
+                "@favoriteBook": "The Gods Themselves",
+                "@age": "21"
+            }
+        }
+    }
+}
+    )";
+    }
+
+    // Consume json to build xml:
+    JsonSaxConsumer consumer;
+    bool success = nlohmann::json::sax_parse(jsonStr, &consumer);
+
+    if (!success)
+        std::cerr << "Conversion error !" << std::endl;
+
+    // output xml
+    std::cout << consumer.getResult().str();
+
+    // exit status
+    return success ? 0:1;
+}
+
